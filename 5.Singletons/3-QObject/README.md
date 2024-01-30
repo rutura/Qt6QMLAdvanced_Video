f23ec6c40d02d5e8ce846f38dba974b1f99acf78
Koech <koech@novek.io>
2024-01-30 13:28:57 +0300

    (5.Singletons): updated and documented lecture 3-QObjects

---
+++ 5.Singletons/3-QObject/README.md
@@ -0,0 +1,11 @@
+# 3-QObject 
+
+- Exposing ```Q_PROPERTY``` based data to propagate changes
+    easily to QML
+
+- Good for data that changes frequently
+
+
+## Changes 
+
+- refactored the approach to singleton design/singleton components. 
--- 5.Singletons/3-QObject/main.cpp
+++ 5.Singletons/3-QObject/main.cpp
@@ -1,6 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include "singletonclass.h"
+// #include "singletonclass.h"
 
 
 int main(int argc, char *argv[])
@@ -12,17 +12,17 @@ int main(int argc, char *argv[])
                                                  SingletonClass::singletonProvider);
                                                  */
 
-    qmlRegisterSingletonType<SingletonClass>("com.blikoon1.singletondata1", 1, 0, "MyApi",
-                                             [](QQmlEngine *engine, QJSEngine *scriptEngine)->QObject *{
-        Q_UNUSED(engine)
-        Q_UNUSED(scriptEngine)
+    // qmlRegisterSingletonType<SingletonClass>("com.blikoon1.singletondata1", 1, 0, "MyApi",
+    //                                          [](QQmlEngine *engine, QJSEngine *scriptEngine)->QObject *{
+    //     Q_UNUSED(engine)
+    //     Q_UNUSED(scriptEngine)
 
-        SingletonClass * example = new SingletonClass();
-        return example;
-    });
+    //     SingletonClass * example = new SingletonClass();
+    //     return example;
+    // });
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/3-QObject/main.qml"_qs);
+    const QUrl url(u"qrc:/com/blikoon1/singletondata1/main.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
